77a924b34438e65f01b5e201d5fdcba323281a1b
Greta Braun <greta@falcon.localdomain>
2017-12-21 11:11:44 +0100

    add author and author_email

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from distutils.core import setup
 setup(
     name = 'pypi_test',
     packages = ['pypi_test'],
-    version = '0.1b',
+    version = '0.2',
     description = 'description',
-    author = '',
-    author_email = '',
+    author = 'Greta Denisenko',
+    author_email = 'greta.denisenko@ambrosys.de',
     url = 'https://github.com/gretade/pyPI_test',
-    download_url = 'https://github.com/gretade/pyPI_test/tarball/0.1b',
+    download_url = 'https://github.com/gretade/pyPI_test/tarball/0.2',
     keywords = ['tag1', 'tag2'],
     classifiers = [],
 )
