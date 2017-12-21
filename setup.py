4981637e619ce21677ad7548867c5185f2f70a1e
Greta Braun <greta@falcon.localdomain>
2017-12-21 11:08:50 +0100

    prep for 0.1b release

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from distutils.core import setup
 setup(
     name = 'pypi_test',
     packages = ['pypi_test'],
-    version = '0.1',
+    version = '0.1b',
     description = 'description',
     author = '',
     author_email = '',
     url = 'https://github.com/gretade/pyPI_test',
-    download_url = 'https://github.com/gretade/pyPI_test/tarball/0.1',
+    download_url = 'https://github.com/gretade/pyPI_test/tarball/0.1b',
     keywords = ['tag1', 'tag2'],
     classifiers = [],
 )
